3c60672c42696b60528cf6e1570aa2c51a02b588
Master-Victor <vvillafane@frba.utn.edu.ar>
2021-10-20 03:06:18 -0300

    starts added

--- src/Component/CardListMovie.js
+++ src/Component/CardListMovie.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react'
-import { Flex } from '@chakra-ui/react'
+import { Flex, Box } from '@chakra-ui/react'
 import CardMovie from './CardMovie'
 import { useSelector } from 'react-redux'
 import { Text } from '@chakra-ui/react'
-import { StarIcon } from '@chakra-ui/icons'
 import StarRatings from 'react-star-ratings'
 const CardListMovie = () => {
 
     const movies = useSelector(store => store.Movies.movies)
     const [ranking, setRanking] = useState();
+
+    const rankingSubmit = (e) => {
+        if( e === ranking ){
+            setRanking(0)
+        }else{
+            setRanking(e)
+        }
+    }
 
     return (
         <Flex wrap="wrap" >
@@ -16,15 +23,17 @@ const CardListMovie = () => {
                 as='h2' fontSize={{ base: '25px', md: '30px' }} fontWeight='500'
                 color='#eee' zIndex='1' mt='20' ml="10" position='absolute'
             >
+                <Box mt="-5" >
                 <StarRatings
                     rating={ranking}
                     starRatedColor="yellow"
-                    changeRating={setRanking}
+                    changeRating={rankingSubmit}
                     numberOfStars={5}
                     name='rating'
                     starDimension="3vw"
                     starSpacing="1vw"
                 />
+                </Box>
             </Text>
             {movies.map((x) => {
                 return <CardMovie key={x.id} name={x.original_title || x.name} image={x.poster_path || x.logo_path} id={x.id} />
